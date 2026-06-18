29963efa44790ef5a2b4334f19fb65008b2705d3
mahakagarwal7 <mahakagrwl07@gmail.com>
2026-06-18 11:57:15 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 import sys
+import os
 
 BUILTINS = ["echo", "exit", "type"]
+
+
+def find_executable(command):
+    path_dirs = os.environ.get("PATH", "").split(os.pathsep)
+
+    for directory in path_dirs:
+        full_path = os.path.join(directory, command)
+
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+
+    return None
 
 
 def main():
@@ -29,7 +42,12 @@ def main():
             if target in BUILTINS:
                 print(f"{target} is a shell builtin")
             else:
-                print(f"{target}: not found")
+                executable = find_executable(target)
+
+                if executable:
+                    print(f"{target} is {executable}")
+                else:
+                    print(f"{target}: not found")
 
         else:
             print(f"{command}: command not found")
